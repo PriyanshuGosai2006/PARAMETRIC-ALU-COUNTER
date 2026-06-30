4c3c787169eca901300b5097d213ce6eb88bfe1f
PriyanshuGosai2006 <priyanshugosai2006@gmail.com>
2026-07-01 00:33:17 +0530

    Update counter_4bit.sv

--- counter_4bit.sv
+++ counter_4bit.sv
@@ -4,12 +4,10 @@ module counter_4bit (
     input  logic       en,
     output logic [3:0] count
 );
-
-    // always_ff is strictly for sequential logic (Flip-Flops).
-    // It tells the synthesis tool to expect a clock and a flip-flop structure.
+    
     always_ff @(posedge clk or negedge rst_n) begin
         if (!rst_n) begin
-            count <= 4'b0000; // Non-blocking assignment (<=) for sequential logic
+            count <= 4'b0000; 
         end else if (en) begin
             count <= count + 1'b1;
         end
